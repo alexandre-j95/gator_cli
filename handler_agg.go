f02000e534f9ba5f860f1c73716306b0b3142b72
Alexandre Jose Afonso Jesus <alexjesus1995@gmail.com>
2026-09-04 15:16:12 +0100

    CH4 L1: Feed aggregating logic now runs on a time interval gated loop

--- handler_agg.go
+++ handler_agg.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
+	"time"
+
 )
 
 func handlerAgg(s *state, cmd command) error {
-	rss, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.args) == 0 {
+		return errors.New("Missing time interval")
+	}
+	
+	time_between_reqs, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%+v\n", rss)
+	
+	fmt.Println()
+	fmt.Printf("Collecting feeds every %s\n", time_between_reqs)
+	fmt.Println()
+	ticker := time.NewTicker(time_between_reqs)
+	for ; ; <-ticker.C {
+		err = scrapeFeeds(s)
+		if err != nil {
+			log.Printf("%v\n", err)
+			continue
+		}
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil { return err }
+
+	_, err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil { return err }
+
+	rssfeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil { return err}
+
+	for _, item := range rssfeed.Channel.Item {
+		fmt.Printf("%+v\n", item.Title)
+	}
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,14 @@ ON feeds.user_id = users.id;
 -- name: GetFeedFromURL :one
 SELECT * FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP
+WHERE id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
